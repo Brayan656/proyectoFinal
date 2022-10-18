7dc7d8345342e2068c095d5b4b7a9986730ad442
Brayan <bcardenas656@gmail.com>
2022-10-18 16:51:15 -0500

    algo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-<<<<<<< HEAD
 import { RegistroComponent } from './pages/registro/registro.component';
-
-const routes: Routes = [
-  {path:"login",component:LoginComponent},
-  {path:"registro",component:RegistroComponent},
-=======
 import { AddComponent } from './pages/products/add/add.component';
 
 const routes: Routes = [
   {path:"login",component:LoginComponent},
+  {path:"registro",component:RegistroComponent},
   {path:"add", component:AddComponent},
->>>>>>> 0edad32d6d6bfe657d68bef8ea1442a2cfc800e8
   {path:"*",component:LoginComponent}
 ];
 
